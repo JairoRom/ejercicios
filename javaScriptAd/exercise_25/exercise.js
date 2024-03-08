ade0203ca4a78c39f8e9fa4a0edacbac7e98758f
unknown <jairo.rom.garzon@gmail.com>
2024-03-08 12:43:53 +0100

    Corregido

--- javaScriptAd/exercise_25/exercise.js
+++ javaScriptAd/exercise_25/exercise.js
@@ -1,5 +1,6 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
 // numberStore.push(newNumber);
-numberStore[numberStore.length]=newNumber;
+numberStore=[...numberStore,newNumber];
+// numberStore[numberStore.length]=newNumber;
 console.log(numberStore);
