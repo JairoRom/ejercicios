da90c1ceb3be8435837f1ef808e674718fc976ff
unknown <jairo.rom.garzon@gmail.com>
2024-03-11 12:40:52 +0100

    corregido

--- javaScriptAd/exercise_28/exercise.js
+++ javaScriptAd/exercise_28/exercise.js
@@ -1,5 +1,10 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+// function sum(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+
+function sum(...numbers) {
+  return numbers.reduce((acc,cvalue)=>acc +cvalue,0)
+  
 }
 
 const numbers = [1, 2, 3];
